6e65f29d173aa39f31ef1344fd2bef41df57ac8b
anish467 <anishagrawal467@gmail.com>
2025-08-19 19:57:29 +0530

    site mp

--- rachana_site/settings.py
+++ rachana_site/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = 'b-5wbbqc7_#2oo4kf=94ri^ev$$wyi8l&+%5a!7cysa@j2xrs4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['rachanacorporation.com','www.rachanacorporation.com','15.207.100.92']
+ALLOWED_HOSTS = ['rachanacorporation.com','www.rachanacorporation.com','13.232.36.238']
 
 #postgresql://rachana_site_user:INmPHRygRWXR8GIPWNThdSoHLLRwWTIB@dpg-d1b40qodl3ps73e8q2ug-a.singapore-postgres.render.com/rachana_site
 
